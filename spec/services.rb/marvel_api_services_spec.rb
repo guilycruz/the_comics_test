7685d18d75fd8cc4fc7afae772f6fdfb2c751f3f
Guilherme Cruz <guilherme_cruz@outlook.com>
2018-02-27 12:13:59 -0300

    Refactoring service spec

--- spec/services.rb/marvel_api_services_spec.rb
+++ spec/services.rb/marvel_api_services_spec.rb
@@ -17,75 +17,52 @@ RSpec.describe MarvelApiService do
       end
     end
 
-    context 'Unauthorized' do
+    context 'Errors'do
+      let(:response) { double('response', code: code, message: '') }
+      let(:params) {
+        {
+          path: 'path',
+          name: 'name'
+        }
+      }
+      subject(:call) { MarvelApiService.new.call(params) }
+
       before do
-        code = 401
-        response = double
-        response.stub(:code) { code }
-        response.stub(:message) { '' }
         allow(RestClient).to receive(:get).and_raise(RestClient::ExceptionWithResponse)
         allow_any_instance_of(RestClient::ExceptionWithResponse).to receive(:http_code).and_return(code)
         allow_any_instance_of(RestClient::ExceptionWithResponse).to receive(:response).and_return(response)
       end
 
-      xit 'responds unnauthorized with an HTTP 401 status code' do
-        params = { path: 'path', name: 'name' }
-        response = MarvelApiService.new.call(params)
-        expect(response).to include('error')
-      end
-    end
+      context 'Unauthorized' do
+        let(:code) { 401 }
 
-    context 'Not Found' do
-      before do
-        code = 404
-        response = double
-        response.stub(:code) { code }
-        response.stub(:message) { '' }
-        allow(RestClient).to receive(:get).and_raise(RestClient::ExceptionWithResponse)
-        allow_any_instance_of(RestClient::ExceptionWithResponse).to receive(:http_code).and_return(code)
-        allow_any_instance_of(RestClient::ExceptionWithResponse).to receive(:response).and_return(response)
+        it 'responds unnauthorized with an HTTP 401 status code' do
+          expect(call).to include('error')
+        end
       end
 
-      xit 'responds not found with an HTTP 404 status code' do
-        params = { path: 'path', name: 'name' }
-        response = MarvelApiService.new.call(params)
-        expect(response).to include('error')
-      end
-    end
+      context 'Not Found' do
+        let(:code) { 404 }
 
-    context 'Conflict' do
-      before do
-        code = 409
-        response = double
-        response.stub(:code) { code }
-        response.stub(:message) { '' }
-        allow(RestClient).to receive(:get).and_raise(RestClient::ExceptionWithResponse)
-        allow_any_instance_of(RestClient::ExceptionWithResponse).to receive(:http_code).and_return(code)
-        allow_any_instance_of(RestClient::ExceptionWithResponse).to receive(:response).and_return(response)
+        it 'responds not found with an HTTP 404 status code' do
+          expect(call).to include('error')
+        end
       end
 
-      xit 'responds not found with an HTTP 409 status code' do
-        params = { path: 'path', name: 'name' }
-        response = MarvelApiService.new.call(params)
-        expect(response).to include('error')
-      end
-    end
+      context 'Conflict' do
+        let(:code) { 409 }
 
-    context 'Other errors' do
-      before do
-        code = 500
-        response = double
-        response.stub(:code) { code }
-        response.stub(:message) { '' }
-        allow(RestClient).to receive(:get).and_raise(RestClient::ExceptionWithResponse)
-        allow_any_instance_of(RestClient::ExceptionWithResponse).to receive(:http_code).and_return(code)
-        allow_any_instance_of(RestClient::ExceptionWithResponse).to receive(:response).and_return(response)
+        it 'responds not found with an HTTP 409 status code' do
+          expect(call).to include('error')
+        end
       end
 
-      xit 'responds any other message with any other HTTP status code' do
-        params = { path: 'path', name: 'name' }
-        response = MarvelApiService.new.call(params)
-        expect(response).to include('error')
+      context 'Other errors' do
+        let(:code) { 500 }
+
+        it 'responds any other message with any other HTTP status code' do
+          expect(call).to include('error')
+        end
       end
     end
   end
